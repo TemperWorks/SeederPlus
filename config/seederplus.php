6cb8f15cbaea105abfed58acd1103c83aa3d4c7b
okdewit <okdewit@gmail.com>
2021-02-24 12:04:58 +0100

    Update for Laravel 8 & PHP 8

--- config/seederplus.php
+++ config/seederplus.php
@@ -1,11 +1,18 @@
 <?php
 
+use Temper\SeederPlus\ModelFinders\FindById;
+
 return [
     'snapshot_storage_path' => base_path('vendor/temper/seederplus/storage/snapshots/'),
     'storage_file' => base_path('vendor/temper/seederplus/storage/seederplus.json'),
     'base_snapshot' => base_path('vendor/temper/seederplus/storage/base-snapshot.sql'),
 
+    'seeder_directories' => [
+        database_path().DIRECTORY_SEPARATOR.'seeds', // <= Laravel 7
+        database_path().DIRECTORY_SEPARATOR.'seeders', // >= Laravel 8
+    ],
+
     'relation_finders' => [
-        \Temper\SeederPlus\ModelFinders\FindById::class => 'Find by id'
+        FindById::class => 'Find by id'
     ]
 ];
